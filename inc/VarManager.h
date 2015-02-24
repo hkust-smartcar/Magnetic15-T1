ad3f93d300a9515c17c7c3b72d9652a5e1a040a1
PeterLau <chunkitmax1125@gmail.com>
2015-02-25 05:29:43 +0800

    Update: conveniently add variables to watch list

--- inc/VarManager.h
+++ inc/VarManager.h
@@ -14,9 +14,10 @@
 #include <libsc/k60/system.h>
 #include <libsc/k60/ftdi_ft232r.h>
 #include <libbase/k60/sys_tick.h>
-
-#include <cstdlib>
-#include <functional>
+#include <libsc/k60/jy_mcu_bt_106.h>
+#include <typeinfo>
+#include <string.h>
+#include <cxxabi.h>
 
 using namespace libsc::k60;
 using namespace libbase::k60;
@@ -29,8 +30,15 @@ public:
 	{
 	public:
 
-		ObjMng();
-		~ObjMng();
+		explicit ObjMng(void *pObj, Byte len, const std::string &typeName, const std::string &objName)
+		:
+			obj(pObj),
+			len(len),
+			typeName(typeName),
+			varName(objName)
+		{}
+
+		~ObjMng() {};
 
 		void						*obj;
 		Byte						len;
@@ -41,19 +49,38 @@ public:
 	explicit VarManager(void);
 	~VarManager(void);
 
-	void Init(const FtdiFt232r::OnReceiveListener &oriListener);
 	void Init(void);
+	void Init(const JyMcuBt106::OnReceiveListener &oriListener);
 	void UnInit(void);
 
-	void addSharedVar(void *pObj, const char *pTypeName, Byte size, const char *pVarName);
-	void addWatchedVar(void *pObj, const char *pTypeName, Byte size, const char *pVarName);
+	template<typename ObjType>
+	void addSharedVar(ObjType *sharedObj, std::string s)
+	{
+		if (!isStarted)
+		{
+			std::string tn = std::string(typeid(ObjType).name());
+			ObjMng newObj(sharedObj, sizeof(sharedObj), tn, s);
+			sharedObjMng.push_back(newObj);
+		}
+	}
+
+	template<typename ObjType>
+	void addWatchedVar(ObjType *watchedObj, std::string s)
+	{
+		if (!isStarted)
+		{
+			ObjMng newObj(watchedObj, sizeof(*watchedObj), __cxxabiv1::__cxa_demangle(typeid(ObjType).name(), nullptr, 0, new int(0)), s);
+			watchedObjMng.push_back(newObj);
+		}
+	}
+
 	void sendWatchData(void);
 
 private:
 
-	FtdiFt232r						m_uart;
+	JyMcuBt106						m_uart;
 
-	FtdiFt232r::OnReceiveListener	m_origin_listener;
+	JyMcuBt106::OnReceiveListener	m_origin_listener;
 
 	std::vector<ObjMng>				sharedObjMng;
 	std::vector<ObjMng>				watchedObjMng;
@@ -64,7 +91,8 @@ private:
 	static void listener(const Byte *bytes, const size_t size);
 
 	SysTick::Config getTimerConfig(void);
-	FtdiFt232r::Config getUartConfig(const uint8_t id);
+	JyMcuBt106::Config get106UartConfig(const uint8_t id);
+	FtdiFt232r::Config get232UartConfig(const uint8_t id);
 
 	void sendWatchedVarInfo(void);
 	void sendSharedVarInfo(void);
