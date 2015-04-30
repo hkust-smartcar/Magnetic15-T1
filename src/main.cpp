26cd68a2f063cd33095d2e842b6349c89249d64f
wuyue <wuyue@wf227-173.ust.hk>
2015-04-30 20:29:06 +0800

    ultrosonic try try

--- src/main.cpp
+++ src/main.cpp
@@ -6,56 +6,64 @@
  */
 
 #include <cstring>
-#include <libbase/k60/mcg.h>
-#include <libsc/us_100.h>
-#include <libsc/system.h>
 #include "MySmartCar.h"
-
+#include <libsc/led.h>
+#include <libsc/system.h>
+#include <libsc/us_100.h>
+#include <libsc/lcd_console.h>
+#include <libsc/st7735r.h>
+#include <libbase/k60/mcg.h>
+#include <libbase/k60/uart.h>
+using namespace std;
+using namespace libsc;
+using namespace libbase::k60;
 namespace libbase
 {
-	namespace k60
-	{
-
-		Mcg::Config Mcg::GetMcgConfig()
-		{
-			Mcg::Config config;
-			config.external_oscillator_khz = 50000;
-			config.core_clock_khz = 150000;
-			return config;
-		}
-
-	}
+namespace k60
+{
+Mcg::Config Mcg::GetMcgConfig()
+{	Mcg::Config config;
+config.external_oscillator_khz = 50000;
+config.core_clock_khz = 150000;
+return config;
+}
+}
 }
 
-using namespace libsc;
-
-int main(void)
-{
+int main(){
 	System::Init();
 
-	MySmartCar myCar;
+	Us100::Config us_config;
+	us_config.id = 0;
+	Us100 us(us_config);
 
-	myCar.m_lcdConsole.setRow(0) << "Similar to Task 1, but the input now is a long integer. Do not enter a number that is \"too long\"" << MyLcd::endl << "Hello";
+	Led::Config led_config;
+	led_config.id = 0;
+	led_config.is_active_low = 0;
+	Led led(led_config);
 
-	float i = 55.5;
-	int16_t j = 120;
 
-	myCar.m_varMng.Init();
-	myCar.m_varMng.addWatchedVar(&i);
-	myCar.m_varMng.addSharedVar(&i, "Lincoln");
-	myCar.m_varMng.addWatchedVar(&j);
-	myCar.m_varMng.addSharedVar(&j, "Peter");
-	myCar.m_varMng.addSharedVar(&j, "Peter2");
-	myCar.m_varMng.addSharedVar(&j, "Peter3");
-	myCar.m_varMng.addSharedVar(&j, "Peter4");
-	myCar.m_varMng.addSharedVar(&j, "Peter5");
-	myCar.m_varMng.addSharedVar(&j, "Peter6");
-	myCar.m_varMng.addSharedVar(&j, "Peter5");
+	St7735r::Config config9;
+	config9.fps = 60;
+	config9.is_bgr = true;
+	St7735r lcd(config9);
 
-	while (true)
-	{
-		myCar.m_varMng.sendWatchData();
-		System::DelayMs(10);
+	LcdConsole::Config config8;
+	config8.lcd = &lcd;
+	LcdConsole console(config8);
+
+	char *buffer = new char[500] {0};
+	us.Start();
+	int distance = 0;
+	lcd.Clear();
+	while(true){
+		us.Start();
+		distance = us.GetDistance();
+		sprintf(buffer," %d",distance);
+		console.WriteString((char*)buffer);
+		if(us.GetDistance() == 0){
+			led.Switch();
+			System::DelayS(1);
+		}
 	}
-
 }
